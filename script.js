1b95f7982ec9ab423323bac8d645befa43e36d5a
FabAlien <93166362+FabAlien@users.noreply.github.com>
2024-07-30 14:32:50 +0200

    Emergency Commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function burgerFunction() {
-  var x = document.getElementById('myNavbar');
-  if (x.className === 'navbar') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navbar';
-  }
-}
